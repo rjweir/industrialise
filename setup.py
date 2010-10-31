1431643bac4ff67f115bd622af91ade160fa0cdc
Rob Weir <rweir@ertius.org>
2010-10-31 14:06:52 +1100

    skeleton

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from industrialise._version import VERSION
+
+setup(name='Industrialise',
+      version=VERSION,
+      description='lxml.html-powered web automation',
+      author='Rob Weir',
+      author_email='rweir@ertius.org',
+      url='http://ertius.org/code/industrialise/',
+      packages=['industrialise'],
+     )
