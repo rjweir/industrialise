cb510dcf1efc5aaff44a33c81b3e22b02f41d5eb
Rob Weir <rweir@ertius.org>
2010-11-01 14:26:42 +1100

    some stuff

--- industrialise/browser.py
+++ industrialise/browser.py
@@ -3,13 +3,25 @@
 import urllib2
 import cookielib
 
+from lxml.html import fromstring
+
 class Browser(object):
     """A pretend browser.  Holds state for a browsing session."""
 
     def __init__(self):
         self._cookiejar = cookielib.CookieJar()
         self._opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self._cookiejar))
+        self._cur_url = None
+        self._cur_page = None
+
+    def _load_data(self, url):
+        return self._opener.open(url).read()
 
     def go(self, url):
         """Visit the provided url."""
+        self._cur_url = url
+        self._cur_page = self._load_data(url)
+        self._tree = fromstring(self._cur_page)
 
+    def find(self, path):
+        return self._tree.xpath(path)
--- industrialise/tests/test_browser.py
+++ industrialise/tests/test_browser.py
@@ -1,7 +1,26 @@
 import unittest
+import os
 
 from industrialise import browser
 
 class TestBrowser(unittest.TestCase):
     def test_instantiate(self):
         browser.Browser()
+
+    def test_load_data(self):
+        b = browser.Browser()
+        data = b._load_data("file://%s/industrialise/tests/valid_html5.html" % os.getcwd())
+        self.assertEqual(data, open("industrialise/tests/valid_html5.html").read())
+
+    def test_go(self):
+        b = browser.Browser()
+        b.go("file://%s/industrialise/tests/valid_html5.html" % os.getcwd())
+
+    def test_finding_something_that_exists(self):
+        b = browser.Browser()
+        b.go("file://%s/industrialise/tests/valid_html5.html" % os.getcwd())
+        self.assertEqual(b.find('//h1')[0].text, "a title")
+
+class TestWithServer(unittest.TestCase):
+    def setUp(self):
+        
