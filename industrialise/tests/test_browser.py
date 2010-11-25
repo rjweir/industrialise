cbf7106d2c88a14569634a80904df2fa96e9c04a
Rob Weir <rweir@ertius.org>
2010-11-25 18:28:32 +1100

    factor out

--- industrialise/tests/test_browser.py
+++ industrialise/tests/test_browser.py
@@ -4,6 +4,9 @@ import os
 import cgi
 from wsgi_intercept.test_wsgi_app import simple_app
 
+def build_url_for_file(name):
+    return "file://%s/industrialise/tests/%s" % (os.getcwd(), name)
+
 class TestBrowser(unittest.TestCase):
 
     def test_provide_cookiejar(self):
@@ -19,15 +22,15 @@ class TestBrowser(unittest.TestCase):
     def test_load_data(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html"
-        data, final_url = b._load_data(url % os.getcwd())
+        url = build_url_for_file("valid_html5.html")
+        data, final_url = b._load_data(url)
         self.assertEqual(data, open("industrialise/tests/valid_html5.html").read())
-        self.assertEqual(final_url, url % os.getcwd())
+        self.assertEqual(final_url, url)
 
     def test_going_sets_url_and_loads_page(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
         b.go(url)
         self.assertEqual(b.url, url)
         self.assertEqual(b.contents, open(url[6:]).read())
@@ -35,34 +38,34 @@ class TestBrowser(unittest.TestCase):
     def test_dupe_links(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
         b.go(url)
         self.assertRaises(ValueError, b.follow, "matchme")
 
     def test_nonexistent_links(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
         b.go(url)
         self.assertRaises(ValueError, b.follow, "dontmatchme")
 
     def test_finding_something_that_exists(self):
         from industrialise import browser
         b = browser.Browser()
-        b.go("file://%s/industrialise/tests/valid_html5.html" % os.getcwd())
+        b.go(build_url_for_file("valid_html5.html"))
         self.assertEqual(b.find('//h1')[0].text, "a title")
 
     def test_finding_something_that_does_not_exist(self):
         from industrialise import browser
         b = browser.Browser()
-        b.go("file://%s/industrialise/tests/valid_html5.html" % os.getcwd())
+        b.go(build_url_for_file("valid_html5.html"))
         self.assertEqual(b.find('//h2'), [])
 
     def testhistory_gets_updated(self):
         from industrialise import browser
         b = browser.Browser()
-        url1 = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
-        url2 = "file://%s/industrialise/tests/invalid_html5.html" % os.getcwd()
+        url1 = build_url_for_file("valid_html5.html")
+        url2 = build_url_for_file("invalid_html5.html")
         b.go(url1)
         b.go(url2)
         self.assertEqual(b.history, [url1, url2])
@@ -70,7 +73,7 @@ class TestBrowser(unittest.TestCase):
     def test_visit_once(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
         b._visit(url)
         self.assertEqual(b.url, url)
         self.assertEqual(b.contents, open(url[6:]).read())
@@ -78,8 +81,8 @@ class TestBrowser(unittest.TestCase):
     def test_visiting_moves(self):
         from industrialise import browser
         b = browser.Browser()
-        url1 = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
-        url2 = "file://%s/industrialise/tests/invalid_html5.html" % os.getcwd()
+        url1 = build_url_for_file("valid_html5.html")
+        url2 = build_url_for_file("invalid_html5.html")
         b.go(url1)
         b.go(url2)
         self.assertEqual(b.url, url2)
@@ -88,8 +91,8 @@ class TestBrowser(unittest.TestCase):
     def test_step_in_previous_river(self):
         from industrialise import browser
         b = browser.Browser()
-        url1 = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
-        url2 = "file://%s/industrialise/tests/invalid_html5.html" % os.getcwd()
+        url1 = build_url_for_file("valid_html5.html")
+        url2 = build_url_for_file("invalid_html5.html")
         b.go(url1)
         b.go(url2)
         b.back()
@@ -99,7 +102,7 @@ class TestBrowser(unittest.TestCase):
     def test_reload(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
         b.go(url)
         t = b._tree
         b.reload()
@@ -108,8 +111,8 @@ class TestBrowser(unittest.TestCase):
     def test_follow_link(self):
         from industrialise import browser
         b = browser.Browser()
-        url = "file://%s/industrialise/tests/valid_html5.html" % os.getcwd()
-        next_url = "file://%s/industrialise/tests/invalid_html5.html" % os.getcwd()
+        url = build_url_for_file("valid_html5.html")
+        next_url = build_url_for_file("invalid_html5.html")
         b.go(url)
         t = b._tree
         b.follow("over there")
@@ -282,7 +285,7 @@ class TestPosting(unittest.TestCase):
     def test_simple_post(self):
         username = "DAUSER"
         b = self._getBrowser()
-        url = "file://%s/industrialise/tests/localform.html" % os.getcwd()
+        url = build_url_for_file("localform.html")
         b.go(url)
         form = b._tree.forms[0]
         form.fields["username"] = username
@@ -341,7 +344,7 @@ class TestPosting(unittest.TestCase):
     def test_GET_form(self):
         username = "DAUSER"
         b = self._getBrowser(WSGIHeaderCapturer)
-        url = "file://%s/industrialise/tests/localform.html" % os.getcwd()
+        url = build_url_for_file("localform.html")
         b.go(url)
         url = "http://localhost/"
         b._tree.make_links_absolute(url, resolve_base_href=True)
